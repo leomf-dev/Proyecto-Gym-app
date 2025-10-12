2cadea1e054fc78356d006e0c32a347a3eaa836b
leomf-dev <leonelmelendezfalcon@gmail.com>
2025-10-12 13:33:28 -0500

    add2

--- app/src/main/java/com/cibertec/proygym/MainActivity.kt
+++ app/src/main/java/com/cibertec/proygym/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.cibertec.proygym
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var CambioAct : Button?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        CambioAct = findViewById(R.id.btnCambioAct)
+
+        CambioAct?.setOnClickListener {
+            cambioActivity(AccesoActivity::class.java)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun cambioActivity(activityDestino : Class<out Activity>) {
+        val intent = Intent(this, activityDestino)
+        startActivity(intent)
     }
 }
